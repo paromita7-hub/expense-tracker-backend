c82a8438503000a1b207e1e5d68e47a5dc56cf86
Paromita Porel <paromitaporel41@gmail.com>
2025-08-04 15:25:28 +0530

    Fix: corrected User model import path

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // server/controllers/authController.js
-const User = require('../models/User');
+const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
